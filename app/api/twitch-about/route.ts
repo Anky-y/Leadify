5503d6451e5b7622a74956a9f99e204509e5a429
Ank <ankon495@gmail.com>
2025-07-15 16:25:34 +0600

    Update route.ts

--- app/api/twitch-about/route.ts
+++ app/api/twitch-about/route.ts
@@ -215,7 +215,7 @@ export async function POST(req: NextRequest) {
     return new Response(JSON.stringify(return_data), { status: 200 });
   } catch (err) {
     console.log(err);
-    return new Response(JSON.stringify({ error: `Internal server error: ${err}`), { status: 500 });
+    return new Response(JSON.stringify({ error: `Internal server error: ${err}`}), { status: 500 });
   }
 
 }
